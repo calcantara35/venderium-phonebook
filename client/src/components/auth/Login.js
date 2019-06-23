ceb8c9ee345e9ba25a7aa5208211afa979e18f82
Cesar Alcantara <cja5391@psu.edu>
2019-06-22 21:23:08 -0400

    Entire Updated Code
    backend and front end, needs to be changed for it to work with tyler's api

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,84 @@
+import React, { useState, useContext, useEffect } from "react";
+import AlertContext from "../../context/alert/alertContext";
+import AuthContext from "../../context/auth/authContext";
+
+const Login = props => {
+  const alertContext = useContext(AlertContext);
+  const authContext = useContext(AuthContext);
+
+  const { setAlert } = alertContext;
+  const { login, error, clearErrors, isAuthenticated } = authContext;
+
+  //checking if we are authenticated
+  //Invalid Credentials is the error message from routes/auth.js, want to match that same with the register one
+  useEffect(() => {
+    if (isAuthenticated) {
+      props.history.push("/");
+    }
+    if (error === "Invalid Credentials") {
+      setAlert(error, "danger");
+      clearErrors();
+    }
+    // eslint-disable-next-line
+  }, [error, isAuthenticated, props.history]);
+
+  //how we use useState, to add component level state
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+  //destructuring to use as variables
+  const { email, password } = user;
+
+  const onChange = e => setUser({ ...user, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    if (email === "" || password === "") {
+      setAlert("Please fill in all fields", "danger");
+    } else {
+      login({
+        email,
+        password
+      });
+    }
+  };
+
+  return (
+    <div className='form-container'>
+      <h1>
+        Account <span className='text-primary'>Login</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className='form-group'>
+          <label htmlFor='email'>Email Address</label>
+          <input
+            type='email'
+            name='email'
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            value={password}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <input
+          type='submit'
+          value='Login'
+          className='btn btn-primary btn-block'
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Login;
